287fe212e67138c3d2b11b400308159fd9c730cb
root <root@ip-172-31-59-74.ec2.internal>
2017-02-13 07:25:52 +0000

    trying to response with img

--- restapi/apiserver.py
+++ restapi/apiserver.py
@@ -33,25 +33,61 @@ def receiveMsgAndSendBackStuff():
                recipient_id = messaging_event["recipient"]["id"]
                message_text = messaging_event["message"]["text"]
                send_message(sender_id, getNumberOfInstaPosts(message_text))
+               #getInstaImages(sender_id, message_text)
    return "ok"
 
 def send_message(recipient_id, message_text):
     print str("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-    params = {
-	"access_token":"EAASXZB3dJrOMBAHptbpYthEDQENypgXrSbiMM2XuHEAFz2oabH2tH7bczWG1UAmTQGVjqrXt5pzvISU9xVYWgOZAveW5DVOr3AZBNpaCB18RoSva5TDx0ilZBANCSC4kKPSEXpgFji8YAonqEI7LVqsEM1AZASCiR4GDd9QZAyVgZDZD"
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
+    params = {"access_token":"EAASXZB3dJrOMBAHptbpYthEDQENypgXrSbiMM2XuHEAFz2oabH2tH7bczWG1UAmTQGVjqrXt5pzvISU9xVYWgOZAveW5DVOr3AZBNpaCB18RoSva5TDx0ilZBANCSC4kKPSEXpgFji8YAonqEI7LVqsEM1AZASCiR4GDd9QZAyVgZDZD"}
+    headers = {"Content-Type": "application/json"}
+    data = json.dumps({"recipient": {"id": recipient_id},
+        "message": {"text": message_text}
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+### The below is just for fun!
+def getInstaImages(senderID, uid):
+    r = requests.get("https://www.instagram.com/"+uid)
+    c = r.content
+    soup = BeautifulSoup(c, "html.parser")
+    body = soup.find("body")
+    body = body.find("script", {"type":"text/javascript"}).text
+    body = unicodedata.normalize('NFKD', body).encode('ascii','ignore')
+    body_len = len(body)
+    body = body[21:body_len-1]
+    data = json.loads(body)
+    l = data["entry_data"]["ProfilePage"]
+    l = l.pop()
+    numberOfPosts = l["user"]["media"]["count"]
+    listOfPosts = l["user"]["media"]["nodes"]
+    try:
+        latestPost = listOfPosts[0]
+        unixTimestamp = latestPost["date"]
+        code = latestPost["code"]
+        thumbnail_src = latestPost["thumbnail_src"]
+        latestPostTime = datetime.datetime.fromtimestamp(unixTimestamp).strftime('%Y-%m-%d %H:%M:%S')
+        
+        inst_url = "https://www.instagram.com/p/" + code + "/?taken-by=" + uid
+        print inst_url
+        print thumbnail_src
+        print "message constructed\n"
+        params = {"access_token":"EAASXZB3dJrOMBAHptbpYthEDQENypgXrSbiMM2XuHEAFz2oabH2tH7bczWG1UAmTQGVjqrXt5pzvISU9xVYWgOZAveW5DVOr3AZBNpaCB18RoSva5TDx0ilZBANCSC4kKPSEXpgFji8YAonqEI7LVqsEM1AZASCiR4GDd9QZAyVgZDZD"}
+        headers = {"Content-Type":"application/json"}
+        data = json.dumps({
+            "recipient":{id: senderID},
+            "message":{
+               "attachment": {
+                   "type":"image",
+                   "payload":{ "url" : "https://yt3.ggpht.com/-v0soe-ievYE/AAAAAAAAAAI/AAAAAAAAAAA/OixOH_h84Po/s900-c-k-no-mo-rj-c0xffffff/photo.jpg" }
+                }
+            }
+        })
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    except:
+        latestPostTime = "N/A"
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
@@ -77,6 +113,7 @@ def getNumberOfInstaPosts(uid):
     except:
         latestPostTime = "N/A"
     return "Number of Posts:"+str(numberOfPosts)+" Latest Post at:"+latestPostTime
+### The above is just fun! 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
